931423d55d14b3faef11c3f4db76416e2e99f8cf
Franjo Ivankovic <29928096+FranjoIM@users.noreply.github.com>
2020-04-08 16:45:07 -0400

    Update PythonBasicsNumpy.py

--- NeuralNetworksDeepLearning/PythonBasicsNumpy.py
+++ NeuralNetworksDeepLearning/PythonBasicsNumpy.py
@@ -123,9 +123,9 @@ x = np.array([
     [7, 5, 0, 0 ,0]])
 print("softmax(x) = " + str(softmax(x)))
 
-#
-# NEEDS EDITING
-#
+######################################################################
+##################### Time track of for loop #########################
+######################################################################
 
 import time
 
@@ -194,44 +194,39 @@ dot = np.dot(W,x1)
 toc = time.process_time()
 print ("gdot = " + str(dot) + "\n ----- Computation time = " + str(1000*(toc - tic)) + "ms")
 
-# GRADED FUNCTION: L1
+
+######################################################################
+######################## Graded Function L1 ##########################
+######################################################################
+"""
+Arguments:
+yhat -- vector of size m (predicted labels)
+y -- vector of size m (true labels)
+Returns:
+loss -- the value of the L1 loss function defined above
+"""
 
 def L1(yhat, y):
-    """
-    Arguments:
-    yhat -- vector of size m (predicted labels)
-    y -- vector of size m (true labels)
-    
-    Returns:
-    loss -- the value of the L1 loss function defined above
-    """
-    
-    ### START CODE HERE ### (≈ 1 line of code)
-    loss = np.sum(np.abs(y-yhat))
-    ### END CODE HERE ###
-    
+    loss = np.sum(np.abs(y-yhat))    
     return loss
     
 yhat = np.array([.9, 0.2, 0.1, .4, .9])
 y = np.array([1, 0, 0, 1, 1])
 print("L1 = " + str(L1(yhat,y)))
 
-# GRADED FUNCTION: L2
+######################################################################
+######################## Graded Function L2 ##########################
+######################################################################
+"""
+Arguments:
+yhat -- vector of size m (predicted labels)
+y -- vector of size m (true labels)
+Returns:
+loss -- the value of the L2 loss function defined above
+"""
 
 def L2(yhat, y):
-    """
-    Arguments:
-    yhat -- vector of size m (predicted labels)
-    y -- vector of size m (true labels)
-    
-    Returns:
-    loss -- the value of the L2 loss function defined above
-    """
-    
-    ### START CODE HERE ### (≈ 1 line of code)
     loss = np.sum(np.square(y-yhat))
-    ### END CODE HERE ###
-    
     return loss
     
 yhat = np.array([.9, 0.2, 0.1, .4, .9])
